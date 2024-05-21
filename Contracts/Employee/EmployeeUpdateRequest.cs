c9afffdeb735d0fae2261c6eb38087982b45d54a
Aleksa <aleksa.mitrovic2001@gmail.com>
2024-05-21 10:40:10 +0200

    SmallFixes

--- Contracts/Employee/EmployeeUpdateRequest.cs
+++ Contracts/Employee/EmployeeUpdateRequest.cs
@@ -15,27 +15,27 @@ namespace Contracts.Employee
 
         [Required(ErrorMessage = "First Name is required.")]
         [StringLength(50, ErrorMessage = "First Name can't be longer than 50 characters.")]
-         string FirstName ,
+        string FirstName ,
 
         [Required(ErrorMessage = "Last Name is required.")]
         [StringLength(50, ErrorMessage = "Last Name can't be longer than 50 characters.")]
-         string LastName ,
+        string LastName ,
 
         [Required(ErrorMessage = "Address is required.")]
         [StringLength(100, ErrorMessage = "Address can't be longer than 100 characters.")]
-         string Address,
+        string Address,
 
         [Required(ErrorMessage = "ID Number is required.")]
         [StringLength(20, ErrorMessage = "ID Number can't be longer than 20 characters.")]
-         string IDNumber,
+        string IDNumber,
 
         [Required(ErrorMessage = "Days Off Number is required.")]
         [Range(0, 365, ErrorMessage = "Days Off Number must be between 0 and 365.")]
-         int DaysOffNumber,
+        int DaysOffNumber,
 
         [Required(ErrorMessage = "Position ID is required.")]
         [Range(1, int.MaxValue, ErrorMessage = "Position ID must be a positive integer.")]
-         int PositionId
+        int PositionId
     );
 }
 
