8e0f0b1e8fc86d0962a8c6907c94445a4561d94e
Aleksa <aleksa.mitrovic2001@gmail.com>
2024-06-27 19:25:24 +0200

    Blob/Dwonload/Added/AzureServices

---
+++ DataLayer/Repositories/Interfaces/IContractRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataLayer.Models.Contract;
+using DataLayer.Repositories.GenericRepository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repositories.Interfaces
+{
+    public interface IContractRepository : IRepository<UserContract>
+    {
+        //public Task AddContractAsync(UserContract contract);
+    }
+}
